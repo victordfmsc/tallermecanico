2b5d9e6b896614a323e59a12356130fd1f7204c2
victordfmsc <31216151+victordfmsc@users.noreply.github.com>
2026-03-17 13:36:16 +0000

    fix: src/app/calendar/page.tsx

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from "@/components/layout/DashboardLayout";
+import CalendarContent from "@/pages/calendar";
+export default function CalendarPage() {
+  return (
+    <DashboardLayout>
+      <CalendarContent />
+    </DashboardLayout>
+  );
+}
