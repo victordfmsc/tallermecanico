8154b6c04b6f8c800b54947b30eb9d8ffce2636c
victordfmsc <31216151+victordfmsc@users.noreply.github.com>
2026-03-17 13:36:43 +0000

    fix: src/app/technicians/page.tsx

---
+++ src/app/technicians/page.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from "@/components/layout/DashboardLayout";
+import TechniciansContent from "@/pages/technicians";
+export default function TechniciansPage() {
+  return (
+    <DashboardLayout>
+      <TechniciansContent />
+    </DashboardLayout>
+  );
+}
